d024e160c7d8d027da42ad7adca6c58b60154e3b
Inzamam-khan <khanloveekhan>
2024-08-10 21:28:15 +0530

    connection completed

---
+++ server.js
@@ -0,0 +1,37 @@
+const dotenv=require('dotenv')
+
+const authRouter=require('./backend/routes/auth.routes.js')
+const messageRoutes =require('./backend/routes/message.routes.js')
+const {usersRoutes}=require('./backend/routes/users.routes.js')
+
+
+const express =require('express');
+const connectToMongoDb = require('./backend/db/connectToDb.js');
+const cookieParser = require('cookie-parser');
+const protectRoutes=require('./backend/middlewares/protectRoutes.js')
+
+
+
+const app=express();
+dotenv.config();
+const PORT=process.env.PORT || 5000;
+
+app.use(express.static("assets"))
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.listen(PORT,()=>{
+    connectToMongoDb()
+    console.log(`server started at ${PORT}`)
+})
+
+
+
+app.use('/api/auth',authRouter);
+
+app.use('/api/messages',protectRoutes,messageRoutes)
+
+app.use('/api/users',protectRoutes,usersRoutes)
+
+app.get('/',(req,res)=>{res.send(`Server Page ${PORT}`)})
